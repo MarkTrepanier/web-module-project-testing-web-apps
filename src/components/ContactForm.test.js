f2eb174e22b3f5779634023eed629b01a3faf3e2
MarkTrepanier <marktrepanier.cajigas@gmail.com>
2021-09-30 16:05:17 -0400

    test 1 complete

--- src/components/ContactForm.test.js
+++ src/components/ContactForm.test.js
@@ -5,7 +5,7 @@ import userEvent from '@testing-library/user-event';
 import ContactForm from './ContactForm';
 
 test('renders without errors', ()=>{
-    
+    const contactForm = render(<ContactForm/>);
 });
 
 test('renders the contact form header', ()=> {
