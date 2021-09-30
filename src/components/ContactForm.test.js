22f542d219d6c4071625d7edad036d12ce349d9d
MarkTrepanier <marktrepanier.cajigas@gmail.com>
2021-09-30 16:19:33 -0400

    onto test 3. added new errors to be thrown before edit

--- src/components/ContactForm.test.js
+++ src/components/ContactForm.test.js
@@ -9,33 +9,35 @@ test('renders without errors', ()=>{
 });
 
 test('renders the contact form header', ()=> {
-    
+    const contactForm = render(<ContactForm/>);
+    const formHeader = screen.queryByText(/contact form/i);
+    expect(formHeader).toBeTruthy();
 });
 
 test('renders ONE error message if user enters less then 5 characters into firstname.', async () => {
-    
+    throw new Error('not yet tested')
 });
 
 test('renders THREE error messages if user enters no values into any fields.', async () => {
-    
+    throw new Error('not yet tested')
 });
 
 test('renders ONE error message if user enters a valid first name and last name but no email.', async () => {
-    
+    throw new Error('not yet tested')
 });
 
 test('renders "email must be a valid email address" if an invalid email is entered', async () => {
-    
+    throw new Error('not yet tested')
 });
 
 test('renders "lastName is a required field" if an last name is not entered and the submit button is clicked', async () => {
-    
+    throw new Error('not yet tested')
 });
 
 test('renders all firstName, lastName and email text when submitted. Does NOT render message if message is not submitted.', async () => {
-    
+    throw new Error('not yet tested')
 });
 
 test('renders all fields text when all fields are submitted.', async () => {
-    
+    throw new Error('not yet tested')
 });
